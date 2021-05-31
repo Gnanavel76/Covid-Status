6168018546d05d743a40657b29ff31aae1c273f3
Gnanavel <gnanavelpandi123@gmail.com>
2021-05-31 17:07:25 +0530

    Update main.js and index.html file

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,4 +34,28 @@ $(document).ready(function(){
         $('header .menu').toggleClass('active');
         $('header .toggle').toggleClass('active');
     });
+
+    // Fetching data from API
+    let table=$('#covid-data tbody');
+    $.get('https://api.covid19api.com/summary',  function (data) {
+          let countriesArray=data.Countries;
+          if(countriesArray.length!=0){
+            $.each(countriesArray,function(index,value){
+                let row="<tr>";
+                row+="<td>"+value.CountryCode+"</td>";
+                row+="<td>"+value.Country+"</td>";
+                row+="<td>"+value.NewConfirmed+"</td>";
+                row+="<td>"+value.TotalConfirmed+"</td>";
+                row+="<td>"+value.NewDeaths+"</td>";
+                row+="<td>"+value.TotalDeaths+"</td>";
+                row+="<td>"+value.NewRecovered+"</td>";
+                row+="<td>"+value.TotalRecovered+"</td>";
+                row+="<td>"+new Date(value.Date).toLocaleString("en-IN",{day:"2-digit",month:"short",year:"numeric"})+"</td>";
+                row+="</tr>";
+                table.append(row)
+            })
+          }else{
+            table.append("<td colspan='9'>No Records</td>")
+          }
+    });
 });
